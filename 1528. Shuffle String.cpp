aab0af059de5fa90f4c235da89de5184738a8aee
Mayank Rawat <52965332+m-rawat@users.noreply.github.com>
2021-04-17 00:11:45 +0530

    Add files via upload

---
+++ 1528. Shuffle String.cpp
@@ -0,0 +1,21 @@
+1528. Shuffle String
+
+https://leetcode.com/problems/shuffle-string/
+
+solution:
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        vector<pair<int,char>> v;
+        for(int i=0;i<s.length();i++){
+            v.push_back(make_pair(indices[i],s[i]));
+        }
+        sort(v.begin(),v.end());
+        string ans="";
+        for(int i=0;i<v.size();i++){
+            ans+=v[i].second;
+        }
+        return ans;
+    }
+};
